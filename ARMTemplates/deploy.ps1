52e80041ef5b04a6c72c81505eadeb021f7553c5
saanvikitnov2022 <saanvikitnov2022@gmail.com>
2023-02-27 16:31:43 +0100

    ARM VM

--- ARMTemplates/deploy.ps1
+++ ARMTemplates/deploy.ps1
@@ -1,5 +1,5 @@
 #Login-AzAccount 
 
-#New-AzResourceGroup -Name "ARMTemplates" -Location "CentralIndia"
+New-AzResourceGroup -Name "ARMTemplates" -Location "CentralIndia"
 
 New-AzResourceGroupDeployment -ResourceGroupName "ARMTemplates" -TemplateFile .\template.json -TemplateParameterFile .\parameters.json -Verbose
