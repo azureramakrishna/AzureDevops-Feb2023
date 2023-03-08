241865862055b7a0a6a63e2745e7d1a391a7d9a5
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2023-03-08 17:09:19 +0100

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,32 +1,4 @@
-  terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm"
-        version = "3.45.0"
-      }
-    }
-  }
-
-  provider "azurerm" {
-    features {}
-
-  client_id       = var.client_id
-  client_secret   = var.secret_value
-  tenant_id       = var.tenannt_id
-  subscription_id = var.subscription_id
-}
-
-  terraform {
-    backend "azurerm" {
-      resource_group_name  = "cloud-shell-storage-centralindia"
-      storage_account_name = "csg10032000825eeb72"
-      container_name       = "tfstate"
-      key                  = "multisa.terraform.tfstate"
-    }
-  }
-
-
-  # Create a resource group
+ # Create a resource group
   resource "azurerm_resource_group" "rg1" {
     name     = var.resource_group_name_01
     location = var.location
